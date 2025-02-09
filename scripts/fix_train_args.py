fbaf352481f9d102fed8667323f837c0a3d76ff4
Pierre Jiang <pierre.primary@qq.com>
2025-02-10 04:01:04 +0800

    优化模型路径输入验证，确保模型文件路径有效

--- scripts/fix_train_args.py
+++ scripts/fix_train_args.py
@@ -1,21 +1,25 @@
+import os
 import torch
 
-default_model_path = "outputs/face-detect/train/weights/last.pt"
-
-model_path = input("请输入模型文件路径: [" + default_model_path + "]").strip()
+model_path = input("请输入模型文件路径: ").strip()
 
 if not model_path:
-    model_path = default_model_path
+    print("模型文件路径不能为空")
+    exit(1)
+
+if not os.path.exists(model_path):
+    print("模型文件不存在")
+    exit(1)
 
 model = torch.load(model_path, weights_only=False)
 
 model["train_args"].update(
     {
-        "model": "outputs/face-detect/train/weights/last.pt",
-        "data": "datasets/wider-face.yaml",
-        "project": "outputs/face-detect",
-        "resume": "outputs/face-detect/train/weights/last.pt",
-        "save_dir": "outputs/face-detect/train",
+        # "model": "outputs/face-detect/train/weights/last.pt",
+        # "data": "datasets/wider-face.yaml",
+        # "project": "outputs/face-detect",
+        # "resume": "outputs/face-detect/train/weights/last.pt",
+        # "save_dir": "outputs/face-detect/train",
     }
 )
 
