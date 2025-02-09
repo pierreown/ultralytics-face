1d10b8eafd1dad1274cf7f3c894653268c30f5ac
Pierre Jiang <pierre.primary@qq.com>
2025-02-09 22:12:46 +0800

    更新训练脚本以支持选择是否恢复上次训练，并调整批量大小

--- train.py
+++ train.py
@@ -7,21 +7,30 @@ params = {
     "epochs": 100,
     "imgsz": 640,
     "optimizer": "SGD",
-    "workers": 12,
-    "batch": 0.95,
+    "batch": 0.90,
     "device": 0,
 }
 
 pre_train = "weights/yolo11n.pt"
-resume_train = os.path.join(params["project"], "train", "weights", "last.pt")
+last_model = os.path.join(params["project"], "train", "weights", "last.pt")
 
 
 def main():
-    # 加载预训练模型
-    model = YOLO(resume_train)
+    model, resume = None, None
+    if not os.path.exists(last_model):
+        resume = False
+    else:
+        resume = input("是否恢复上次训练？(y/n) [y]").strip().lower()
+        resume = resume not in ["n", "no"]
+
+    # 加载模型
+    if resume:
+        model = YOLO(last_model)
+    else:
+        model = YOLO(pre_train)
 
     # 训练模型
-    model.train(**params, resume=True)
+    model.train(**params, resume=resume)
 
 
 if __name__ == "__main__":
